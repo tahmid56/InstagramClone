c8749c24419ec6fa1002035eba23267e465649b2
Tahmid Akter <tahmidakter56@gmail.com>
2023-10-09 14:49:52 +0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Tahmid Akter on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
