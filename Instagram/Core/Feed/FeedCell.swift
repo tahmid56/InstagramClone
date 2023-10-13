1c0ac08ab1774b4043b54d5e3bbcf8fb3e11a7a0
Tahmid Akter <tahmidakter56@gmail.com>
2023-10-13 21:24:09 +0600

    post model implemented in feedcell

--- Instagram/Core/Feed/FeedCell.swift
+++ Instagram/Core/Feed/FeedCell.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct FeedCell: View {
+    let post: Post
     var body: some View {
         VStack{
             //image + username
             HStack{
-                Image("panther")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .clipShape(Circle())
-                Text("batman")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
+                if let user = post.user {
+                    Image(user.profileImageUrl ?? "")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle())
+                    
+                    Text(user.username)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                
                 Spacer()
             }
             .padding(.leading, 8)
             //Post image
-            Image("panther")
+            Image(post.imageUrl)
                 .resizable()
                 .scaledToFill()
                 .frame(height: 400)
@@ -61,7 +66,7 @@ struct FeedCell: View {
             
             //likes label
             
-            Text("23 likes")
+            Text("\(post.likes) likes")
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -71,9 +76,9 @@ struct FeedCell: View {
             //Caption label
             
             HStack{
-                Text("batman ")
+                Text("\(post.user?.username ?? "") ")
                     .fontWeight(.semibold) +
-                Text("This is some test captain for now")
+                Text(post.caption)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .font(.footnote)
@@ -93,6 +98,6 @@ struct FeedCell: View {
 
 struct FeedCell_Previews: PreviewProvider {
     static var previews: some View {
-        FeedCell()
+        FeedCell(post: Post.MOCK_POSTS[0])
     }
 }
